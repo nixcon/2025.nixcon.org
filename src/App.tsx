30860a41d4fff0ea3b1fc1d53a5f2673cd052e73
Enrico Scherlies <e.scherlies@pm.me>
2025-03-27 17:06:56 +0100

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div class="flex min-h-screen">
+        <section class="m-auto">
+          <h1 class="font-bold text-6xl uppercase">NixCon 2025</h1>
+        </section>
+      </div>
+    </>
+  )
+}
+
+export default App

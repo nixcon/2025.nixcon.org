08cedb355ae2789bedf74fe2aef26b59fb262337
Enrico Scherlies <e.scherlies@pm.me>
2025-05-28 21:08:42 +0200

    Add Code of Conduct page
    - Remove the downloadable PDF version of the Code of Conduct
    - Create a new web page at `/coc` to display the Code of Conduct directly
    - Update navigation links on mobile and desktop menus
    - Update the home page link to view the new Code of Conduct page

--- src/components/TopMenu.jsx
+++ src/components/TopMenu.jsx
@@ -38,18 +38,16 @@ export default function TopMenu() {
     <div class="fixed top-0 left-0 right-0 z-50 hidden md:block">
       <div class="max-w-3xl mx-auto p-2.5">
         <div
-          class={`flex items-center justify-center h-16 ${
-            scrolled() ? "glass" : ""
-          }`}
+          class={`flex items-center justify-center h-16 ${scrolled() ? "glass" : ""
+            }`}
         >
           {/* Navigation Links */}
           <nav class="flex items-center space-x-10">
             {/* Home with Logo */}
             <A
               href="/"
-              class={`text-white hover:text-white/80 transition-all duration-75 ${
-                isActive("/") ? "underline" : ""
-              }`}
+              class={`text-white hover:text-white/80 transition-all duration-75 ${isActive("/") ? "underline" : ""
+                }`}
               aria-label="Home"
             >
               <div class="h-6">
@@ -60,9 +58,8 @@ export default function TopMenu() {
             {/* Sponsorship */}
             <A
               href="/sponsorship"
-              class={`text-white hover:text-white/80 transition-all duration-75 font-bold text-lg ${
-                isActive("/sponsorship") ? "underline" : ""
-              }`}
+              class={`text-white hover:text-white/80 transition-all duration-75 font-bold text-lg ${isActive("/sponsorship") ? "underline" : ""
+                }`}
               aria-label="Sponsorship"
             >
               Sponsorship
@@ -71,9 +68,8 @@ export default function TopMenu() {
             {/* Accommodation */}
             <A
               href="/hotels"
-              class={`text-white hover:text-white/80 transition-all duration-75 font-bold text-lg ${
-                isActive("/hotels") ? "underline" : ""
-              }`}
+              class={`text-white hover:text-white/80 transition-all duration-75 font-bold text-lg ${isActive("/hotels") ? "underline" : ""
+                }`}
               aria-label="Accommodation"
             >
               Accommodation
@@ -82,12 +78,20 @@ export default function TopMenu() {
             {/* Organizers */}
             <A
               href="/organizers"
-              class={`text-white hover:text-white/80 transition-all duration-75 font-bold text-lg ${
-                isActive("/organizers") ? "underline" : ""
-              }`}
+              class={`text-white hover:text-white/80 transition-all duration-75 font-bold text-lg ${isActive("/organizers") ? "underline" : ""
+                }`}
               aria-label="Organizers"
             >
               Organizers
+            </A>
+            {/* Code of Conduct */}
+            <A
+              href="/coc"
+              class={`text-white hover:text-white/80 transition-all duration-75 font-bold text-lg ${isActive("/coc") ? "underline" : ""
+                }`}
+              aria-label="Code of Conduct"
+            >
+              CoC
             </A>
           </nav>
         </div>

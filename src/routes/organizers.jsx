de39da40c0ad9cceafdfb5c5c44ada3bb59ba4fe
Enrico Scherlies <e.scherlies@pm.me>
2025-06-05 17:53:04 +0200

    Fix duplicate key

--- src/routes/organizers.jsx
+++ src/routes/organizers.jsx
@@ -91,7 +91,6 @@ const organizers = [
     name: "Farhad",
     github: "fmehta",
     githubUrl: "https://github.com/fmehta",
-    matrix: "@farhad_mehta:matrix.org",
     discourse: "fmehta",
     discourseUrl: "https://discourse.nixos.org/u/fmehta",
     matrix: "@farhad_mehta:matrix.org",

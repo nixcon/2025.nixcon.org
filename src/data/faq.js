a4aacb4fd1ce8f3f7b7deb332320e4b30123a1f3
Andreas Zweili <andreas@zweili.ch>
2025-06-12 19:48:29 +0200

    Add FAQ items

--- src/data/faq.js
+++ src/data/faq.js
@@ -1,7 +1,53 @@
 export const faqItems = [
   {
     question: "What is NixCon?",
-    answer: "NixCon is the annual conference for the Nix and NixOS community. It's a place to learn, share, and connect with others interested in Nix."
+    answer:
+      "NixCon is the annual conference for the Nix and NixOS community. It's a place to learn, share, and connect with others interested in Nix.",
+  },
+  {
+    question: "Do I need a visa to attend?",
+    answer:
+      "It depends on your country of origin. Please check this government website for further details: https://www.eda.admin.ch/countries/usa/en/home/visa/entry-ch/up-90-days/do-i-need-a-schengen-visa.html",
+  },
+  {
+    question:
+      "I need a letter of invitation for my visa which information do you need?",
+    answer:
+      "Please provide: - surname - first name - date of birth - address - telephone number - email address - nationality and your NixCon ticket number.",
+  },
+  {
+    question: "Is there parking available?",
+    answer:
+      "The following site (in German) provides a list of all the public parking spaces available in the city. The campus itself doesn't have any long time parking spaces. https://www.rapperswil-jona.ch/parkiermoeglichkeiten",
+  },
+  {
+    question: "How do I plan and book public transport?",
+    answer:
+      "The easiest way is to use the website of federal railway: https://www.sbb.ch/en or their app: https://www.sbb.ch/en/travel-information/apps/sbb-mobile.html",
+  },
+  {
+    question: "What is the cheapest way to travel by public transport?",
+    answer:
+      "For the arival you can try to buy one of the limited day passes for Switzerland: https://www.sbb.ch/en/buying/pages/kontingent/kontingent.xhtml during the conference you can buy a 24h-ticket for the Zürich region: https://www.zvv.ch/en/travelcards-and-tickets/tickets/24h-tickets.html or the 9 o'clock day pass: https://www.zvv.ch/en/travelcards-and-tickets/tickets/9-oclock-day-pass.html. Both are available from SBB as well: https://www.sbb.ch/en",
+  },
+  {
+    question: "What is special about the hotels in the 'Accommodation' menu?",
+    answer:
+      "They are regional hotels which agreed to give us a discount and/or reserved a contingent for the conference. They were contacted by us and asked for it.",
+  },
+  {
+    question: "Is there some place I can camp?",
+    answer:
+      "There is a camping place in the city which you have to reserve in advance: https://www.rapperswil-jona.ch/naherholungimfreien/7647",
+  },
+  {
+    question:
+      "I didn't see the voucher and payed for the ticket, what do I do?",
+    answer: "Send an email to nixcon@nixos.org.",
+  },
+  {
+    question: "I can't afford the ticket but I'm not alligable for a ticket?",
+    answer:
+      "Send an email to nixcon@nixos.org and describe your situation and we see what we can do.",
   },
 ];
-

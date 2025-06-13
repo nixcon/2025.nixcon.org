8f29615fe58227d05b35c1582b5dc3b9de78f01b
Enrico Scherlies <e.scherlies@pm.me>
2025-06-13 11:58:22 +0200

    Refactor data management for hotels and organizers
    - Remove `faq.js` file and its content
    - Create `hotels.js` to store hotel deals data
    - Create `organizers.js` to store organizers data
    - Update `hotels.jsx` to import hotel deals from `hotels.js`
    - Update `organizers.jsx` to import organizers from `organizers.js`

--- src/routes/hotels.jsx
+++ src/routes/hotels.jsx
@@ -1,62 +1,11 @@
-import { FaSolidLink } from "@aminya/solid-icons/fa/FaSolidLink"
-import { FaSolidMapLocationDot } from "@aminya/solid-icons/fa/FaSolidMapLocationDot"
 import { For } from "solid-js"
 import { HotelCard } from "~/components/Cards"
 import PageLayout from "~/components/PageLayout"
 import { Section } from "~/components/Section"
-import { Paragraph, SecondaryButtonLink, SubTitle, Title } from "~/components/Ui"
+import { Paragraph, SecondaryButtonLink, Title } from "~/components/Ui"
+import { hotelDeals } from "~/data/hotels"
 
 export default function Hotels() {
-  // Sorted by confirmation date
-  const hotelDeals = [
-    {
-      name: "The Yarn",
-      additionalInformation: "Breakfast is included.",
-      prices: [],
-      travelTime: "Travel time by train can be as low as 30 minutes.",
-      website: "https://theyarn.ch/",
-      mapUrl: "https://maps.app.goo.gl/6a5Cfi281uYqgtpu5",
-      bookingInfo: "Book via email at corporate@theyarn.ch, mentioning you are attending the NixCon conference.",
-      imagePath: "/the-yarn-rates.png",
-      imageAlt: "The Yarn Hotel Room Rates",
-    },
-    {
-      name: "Hotel Swiss Star",
-      additionalInformation: "",
-      prices: ["Single room incl. breakfast CHF 150.00/night", "Double room incl. breakfast CHF 190.00/night"],
-      travelTime: "Travel time by train to OST is about 28 minutes.",
-      website: "https://www.hotel-swiss-star.ch",
-      mapUrl: "https://maps.app.goo.gl/RDurEWjULhBkgX3M7",
-      bookingInfo: 'Book directly on the hotel website using promo code "NixCon".',
-    },
-    {
-      name: "Hotel Residence Loren",
-      additionalInformation:
-        "Underground parking is available for CHF 10 per night. Cancellation is possible until seven days before arrival.",
-      prices: [
-        "Double room for one person incl. breakfast: CHF 125.00/night",
-        "Double room for two persons incl. breakfast: CHF 150.00/night",
-      ],
-      bookingInfo: 'Book via email at info@hotel-residence-loren.ch and mention "NixCon 2025"',
-      website: "https://www.hotel-residence-loren.ch/",
-      mapUrl: "https://maps.app.goo.gl/cZF7fvNNgwutw3qh6",
-      travelTime: "Travel time by train to OST can be as low as 37 minutes.",
-    },
-    {
-      name: "Aabach Hotel",
-      additionalInformation:
-        "The offer is only valid until the 4th of August. Free parking and a gym are available. A restaurant and a bistro with a quiet sun terrasse are available in the same building.",
-      prices: [
-        "Double room for one person incl. breakfast: CHF 145.00/night + CHF 3.5 city tax/night and person",
-        "Double room for two persons incl. breakfast: CHF 160.00/night + CHF 3.5 city tax/night and person",
-      ],
-      bookingInfo: 'Book via email at info@aabach-hotel.ch and mention "NixCon 2025"',
-      website: "https://www.aabach-hotel.ch/",
-      mapUrl: "https://maps.app.goo.gl/J9sjKHhDE2jxT3326",
-      travelTime: "Travel time by train to OST 35 minutes.",
-    },
-  ]
-
   return (
     <div class="relative min-h-svh text-white">
       <PageLayout reducedSpacing={true}>

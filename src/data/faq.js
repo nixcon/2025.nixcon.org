e52a3e9e3f3052e2eaab2a7bd1e16304fd5083dd
Enrico Scherlies <e.scherlies@pm.me>
2025-06-13 11:15:29 +0200

    Add marked and typography support to FAQ
    - Add "marked" and "@tailwindcss/typography" to dependencies
    - Update FAQ answers with markdown links and formatting
    - Import and configure "marked" in faq.jsx for rendering markdown
    - Update app.css to include typography plugin
    - Enhance FAQ page with markdown rendering and improved styling

--- src/data/faq.js
+++ src/data/faq.js
@@ -7,28 +7,37 @@ export const faqItems = [
   {
     question: "Do I need a visa to attend?",
     answer:
-      "It depends on your country of origin. Please check this government website for further details: https://www.eda.admin.ch/countries/usa/en/home/visa/entry-ch/up-90-days/do-i-need-a-schengen-visa.html",
+      "It depends on your country of origin. Please check this [government website for visa information](https://www.eda.admin.ch/countries/usa/en/home/visa/entry-ch/up-90-days/do-i-need-a-schengen-visa.html) for further details.",
   },
   {
-    question:
-      "I need a letter of invitation for my visa which information do you need?",
-    answer:
-      "Please provide: - surname - first name - date of birth - address - telephone number - email address - nationality and your NixCon ticket number.",
+    question: "I need a letter of invitation for my visa which information do you need?",
+    answer: `Please provide:
+- Surname
+- First name
+- Date of birth
+- Address
+- Telephone number
+- Email address
+- Nationality
+- And your NixCon ticket number.`,
   },
   {
     question: "Is there parking available?",
     answer:
-      "The following site (in German) provides a list of all the public parking spaces available in the city. The campus itself doesn't have any long time parking spaces. https://www.rapperswil-jona.ch/parkiermoeglichkeiten",
+      "The following site (in German) provides a [list of public parking spaces](https://www.rapperswil-jona.ch/parkiermoeglichkeiten) available in the city. The campus itself doesn't have any long time parking spaces.",
   },
   {
     question: "How do I plan and book public transport?",
     answer:
-      "The easiest way is to use the website of federal railway: https://www.sbb.ch/en or their app: https://www.sbb.ch/en/travel-information/apps/sbb-mobile.html",
+      "The easiest way is to use the [SBB website](https://www.sbb.ch/en) or their [SBB Mobile app](https://www.sbb.ch/en/travel-information/apps/sbb-mobile.html).",
   },
   {
     question: "What is the cheapest way to travel by public transport?",
-    answer:
-      "For the arival you can try to buy one of the limited day passes for Switzerland: https://www.sbb.ch/en/buying/pages/kontingent/kontingent.xhtml during the conference you can buy a 24h-ticket for the Zürich region: https://www.zvv.ch/en/travelcards-and-tickets/tickets/24h-tickets.html or the 9 o'clock day pass: https://www.zvv.ch/en/travelcards-and-tickets/tickets/9-oclock-day-pass.html. Both are available from SBB as well: https://www.sbb.ch/en",
+    answer: `For the arrival you can try to buy one of the [limited day passes for Switzerland](https://www.sbb.ch/en/buying/pages/kontingent/kontingent.xhtml).
+
+During the conference you can buy a [24h-ticket for the Zürich region](https://www.zvv.ch/en/travelcards-and-tickets/tickets/24h-tickets.html) or the [9 o'clock day pass](https://www.zvv.ch/en/travelcards-and-tickets/tickets/9-oclock-day-pass.html).
+
+Both are available from [SBB](https://www.sbb.ch/en) as well.`,
   },
   {
     question: "What is special about the hotels in the 'Accommodation' menu?",
@@ -38,21 +47,18 @@ export const faqItems = [
   {
     question: "Is there some place I can camp?",
     answer:
-      "There is a camping place in the city which you have to reserve in advance: https://www.rapperswil-jona.ch/naherholungimfreien/7647",
+      "There is a [camping place in the city](https://www.rapperswil-jona.ch/naherholungimfreien/7647) which you have to reserve in advance.",
   },
   {
-    question:
-      "I didn't see the voucher and payed for the ticket, what do I do?",
+    question: "I didn't see the voucher and payed for the ticket, what do I do?",
     answer: "Send an email to nixcon@nixos.org.",
   },
   {
     question: "I can't afford the ticket but I'm not alligable for a ticket?",
-    answer:
-      "Send an email to nixcon@nixos.org and describe your situation and we see what we can do.",
+    answer: "Send an email to nixcon@nixos.org and describe your situation and we see what we can do.",
   },
   {
     question: "Where can I buy groceries?",
-    answer:
-      "There are various stores in the city. The main chains are called 'Migros' and 'Coop'. Right next to the train station is a small 'Coop' that is even open on Sundays: https://www.openstreetmap.org/way/192142413",
+    answer: `There are various stores in the city. The main chains are called *Migros* and *Coop*. Right next to the train station is a small Coop that is even open on Sundays: [Coop location on OpenStreetMap](https://www.openstreetmap.org/way/192142413)`,
   },
-];
+]
--- src/routes/faq.jsx
+++ src/routes/faq.jsx
@@ -1,7 +1,12 @@
+import { marked } from "marked"
 import PageLayout from "~/components/PageLayout"
 import { Section } from "~/components/Section"
-import { Paragraph, SubTitle, Title } from "~/components/Ui"
 import { faqItems } from "~/data/faq.js"
+
+const renderer = new marked.Renderer()
+renderer.link = ({ href, text }) => {
+  return `<a target="_blank" rel="noopener noreferrer" href="${href}">${text}</a>`
+}
 
 export default function FaqPage() {
   return (
@@ -18,7 +23,10 @@ export default function FaqPage() {
               </div>
               <div class="pt-4 text-white/90">
                 {/* Added padding and slightly lighter text for answer */}
-                <Paragraph>{item.answer}</Paragraph>
+                <article
+                  class="prose prose-neutral prose-invert prose-p:text-white prose-li:text-white prose-li:list-['-']  prose-li:leading-4 prose-ul:text-green-400 prose-li:marker:text-white"
+                  innerHTML={marked.parse(item.answer, { async: false, gfm: true, renderer: renderer })}
+                />
               </div>
             </div>
           ))}

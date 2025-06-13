0c60bd89876addfb5be0775a7d6fcf7d5aada48a
Enrico Scherlies <e.scherlies@pm.me>
2025-06-13 11:58:22 +0200

    Refactor data management for hotels and organizers
    - Remove `faq.js` file and its content
    - Create `hotels.js` to store hotel deals data
    - Create `organizers.js` to store organizers data
    - Update `hotels.jsx` to import hotel deals from `hotels.js`
    - Update `organizers.jsx` to import organizers from `organizers.js`

---
+++ src/data/hotels.js
@@ -0,0 +1,66 @@
+/**
+ * @typedef {Object} HotelDeal
+ * @property {string} name - The name of the hotel.
+ * @property {string} additionalInformation - Additional information about the hotel or deal.
+ * @property {string[]} prices - An array of strings describing the prices.
+ * @property {string} travelTime - Information about the travel time to the venue.
+ * @property {string} website - The URL of the hotel's website.
+ * @property {string} mapUrl - The URL to the hotel's location on a map.
+ * @property {string} bookingInfo - Instructions on how to book the deal.
+ * @property {string} [imagePath] - The path to an image for the hotel deal.
+ * @property {string} [imageAlt] - Alt text for the hotel deal image.
+ */
+
+/**
+ * An array of hotel deal objects.
+ * @type {HotelDeal[]}
+ */
+// Sorted by confirmation date
+export const hotelDeals = [
+  {
+    name: "The Yarn",
+    additionalInformation: "Breakfast is included.",
+    prices: [],
+    travelTime: "Travel time by train can be as low as 30 minutes.",
+    website: "https://theyarn.ch/",
+    mapUrl: "https://maps.app.goo.gl/6a5Cfi281uYqgtpu5",
+    bookingInfo: "Book via email at corporate@theyarn.ch, mentioning you are attending the NixCon conference.",
+    imagePath: "/the-yarn-rates.png",
+    imageAlt: "The Yarn Hotel Room Rates",
+  },
+  {
+    name: "Hotel Swiss Star",
+    additionalInformation: "",
+    prices: ["Single room incl. breakfast CHF 150.00/night", "Double room incl. breakfast CHF 190.00/night"],
+    travelTime: "Travel time by train to OST is about 28 minutes.",
+    website: "https://www.hotel-swiss-star.ch",
+    mapUrl: "https://maps.app.goo.gl/RDurEWjULhBkgX3M7",
+    bookingInfo: 'Book directly on the hotel website using promo code "NixCon".',
+  },
+  {
+    name: "Hotel Residence Loren",
+    additionalInformation:
+      "Underground parking is available for CHF 10 per night. Cancellation is possible until seven days before arrival.",
+    prices: [
+      "Double room for one person incl. breakfast: CHF 125.00/night",
+      "Double room for two persons incl. breakfast: CHF 150.00/night",
+    ],
+    bookingInfo: 'Book via email at info@hotel-residence-loren.ch and mention "NixCon 2025"',
+    website: "https://www.hotel-residence-loren.ch/",
+    mapUrl: "https://maps.app.goo.gl/cZF7fvNNgwutw3qh6",
+    travelTime: "Travel time by train to OST can be as low as 37 minutes.",
+  },
+  {
+    name: "Aabach Hotel",
+    additionalInformation:
+      "The offer is only valid until the 4th of August. Free parking and a gym are available. A restaurant and a bistro with a quiet sun terrasse are available in the same building.",
+    prices: [
+      "Double room for one person incl. breakfast: CHF 145.00/night + CHF 3.5 city tax/night and person",
+      "Double room for two persons incl. breakfast: CHF 160.00/night + CHF 3.5 city tax/night and person",
+    ],
+    bookingInfo: 'Book via email at info@aabach-hotel.ch and mention "NixCon 2025"',
+    website: "https://www.aabach-hotel.ch/",
+    mapUrl: "https://maps.app.goo.gl/J9sjKHhDE2jxT3326",
+    travelTime: "Travel time by train to OST 35 minutes.",
+  },
+]
---
+++ src/data/organizers.js
@@ -0,0 +1,166 @@
+/**
+ * @typedef {Object} Organizer
+ * @property {string} name - The name of the organizer.
+ * @property {string} [github] - The GitHub username of the organizer.
+ * @property {string} [githubUrl] - The URL to the GitHub profile of the organizer.
+ * @property {string} matrix - The Matrix ID of the organizer.
+ * @property {string} matrixUrl - The URL to the Matrix profile of the organizer.
+ * @property {string} [discourse] - The Discourse username of the organizer.
+ * @property {string} [discourseUrl] - The URL to the Discourse profile of the organizer.
+ * @property {string[]} responsibilities - An array of responsibilities for the organizer.
+ */
+
+/**
+ * An array of organizer objects.
+ * @type {Organizer[]}
+ */
+export const organizers = [
+  // vv Main organisers vv
+  {
+    name: "Alex",
+    github: "ners",
+    githubUrl: "https://github.com/ners",
+    matrix: "@ners:nixos.dev",
+    matrixUrl: "https://matrix.to/#/%40ners:nixos.dev",
+    discourse: "ners",
+    discourseUrl: "https://discourse.nixos.org/u/ners",
+    responsibilities: ["Program", "Date, venue", "Website"],
+  },
+  {
+    name: "John",
+    github: "john-rodewald",
+    githubUrl: "https://github.com/john-rodewald",
+    matrix: "@john-rodewald:nixos.dev",
+    matrixUrl: "https://matrix.to/#/%40john-rodewald:nixos.dev",
+    discourse: "john-rodewald",
+    discourseUrl: "https://discourse.nixos.org/u/john-rodewald",
+    responsibilities: ["Tickets", "Food", "A/V"],
+  },
+  {
+    name: "Lassulus",
+    github: "lassulus",
+    githubUrl: "https://github.com/lassulus",
+    matrix: "@lassulus:lassul.us",
+    matrixUrl: "https://matrix.to/#/%40lassulus:lassul.us",
+    discourse: "lassulus",
+    discourseUrl: "https://discourse.nixos.org/u/lassulus",
+    responsibilities: ["Program", "Tickets"],
+  },
+  {
+    name: "Silvan",
+    github: "infinisil",
+    githubUrl: "https://github.com/infinisil",
+    matrix: "@infinisil:matrix.org",
+    matrixUrl: "https://matrix.to/#/%40infinisil:matrix.org",
+    discourse: "infinisil",
+    discourseUrl: "https://discourse.nixos.org/u/infinisil",
+    responsibilities: ["General administration", "Hotels", "Sponsorships", "Letters of Invitation"],
+  },
+  // ^^ Main organisers ^^
+  {
+    name: "Andreas",
+    github: "Nebucatnetzer",
+    githubUrl: "https://github.com/Nebucatnetzer",
+    matrix: "@nebucatnetzer13:matrix.org",
+    matrixUrl: "https://matrix.to/#/%40nebucatnetzer13:matrix.org",
+    discourse: "Nebucatnetzer",
+    discourseUrl: "https://discourse.nixos.org/u/Nebucatnetzer",
+    responsibilities: ["Hotels"],
+  },
+  {
+    name: "Andreas",
+    github: "andir",
+    githubUrl: "https://github.com/andir",
+    matrix: "@andi:kack.it",
+    matrixUrl: "https://matrix.to/#/%40andi:kack.it",
+    discourse: "andir",
+    discourseUrl: "https://discourse.nixos.org/u/andir",
+    responsibilities: ["A/V"],
+  },
+  {
+    name: "Berber",
+    github: "zmberber",
+    githubUrl: "https://github.com/zmberber",
+    matrix: "@zmberber:matrix.org",
+    matrixUrl: "https://matrix.to/#/%40zmberber:matrix.org",
+    discourse: "zmberber",
+    discourseUrl: "https://discourse.nixos.org/u/zmberber",
+    responsibilities: ["Letters of Invitation"],
+  },
+  {
+    name: "Enrico",
+    github: "escherlies",
+    githubUrl: "https://github.com/escherlies",
+    matrix: "@enryco:tchncs.de",
+    matrixUrl: "https://matrix.to/#/%40enryco:tchncs.de",
+    responsibilities: ["Website"],
+  },
+  {
+    name: "Farhad",
+    github: "fmehta",
+    githubUrl: "https://github.com/fmehta",
+    discourse: "fmehta",
+    discourseUrl: "https://discourse.nixos.org/u/fmehta",
+    matrix: "@farhad_mehta:matrix.org",
+    matrixUrl: "https://matrix.to/#/%40farhad_mehta:matrix.org",
+    responsibilities: ["Date, venue"],
+  },
+  {
+    name: "hrmny",
+    github: "ForsakenHarmony",
+    githubUrl: "https://github.com/ForsakenHarmony",
+    matrix: "@hrmny:matrix.org",
+    matrixUrl: "https://matrix.to/#/%40hrmny:c-base.org", // Note: Original had matrix.org, then c-base.org. Keeping c-base.org as per last entry.
+    discourse: "hrmny",
+    discourseUrl: "https://discourse.nixos.org/u/hrmny",
+    responsibilities: ["Tickets", "A/V"],
+  },
+  {
+    name: "Kenji",
+    github: "a-kenji",
+    githubUrl: "https://github.com/a-kenji",
+    matrix: "@a-kenji:matrix.org",
+    matrixUrl: "https://matrix.to/#/%40a-kenji:matrix.org",
+    discourse: "a-kenji",
+    discourseUrl: "https://discourse.nixos.org/u/a-kenji",
+    responsibilities: ["Program", "Perk delivery"],
+  },
+  {
+    name: "Picnoir",
+    github: "picnoir",
+    githubUrl: "https://github.com/picnoir",
+    matrix: "@picnoir:alternativebit.fr",
+    matrixUrl: "https://matrix.to/#/%40picnoir:alternativebit.fr",
+    discourse: "picnoir",
+    discourseUrl: "https://discourse.nixos.org/u/picnoir",
+    responsibilities: ["A/V"],
+  },
+  {
+    name: "Ral",
+    // github: "", // No GitHub in original data
+    // githubUrl: "https://github.com/",
+    matrix: "@ral:fairydust.space",
+    matrixUrl: "https://matrix.to/#/%40ral:fairydust.space",
+    responsibilities: ["A/V"],
+  },
+  {
+    name: "Raphael",
+    github: "das-g",
+    githubUrl: "https://github.com/das-g",
+    matrix: "@das-g:matrix.org",
+    matrixUrl: "https://matrix.to/#/%40das-g:matrix.org",
+    discourse: "das-g",
+    discourseUrl: "https://discourse.nixos.org/u/das-g",
+    responsibilities: ["Date, venue"],
+  },
+  {
+    name: "Sebastian",
+    github: "ra33it0",
+    githubUrl: "https://github.com/ra33it0",
+    matrix: "@ra33it0:matrix.org",
+    matrixUrl: "https://matrix.to/#/%40ra33it0:matrix.org",
+    discourse: "ra33it0",
+    discourseUrl: "https://discourse.nixos.org/u/ra33it0",
+    responsibilities: ["Program", "Sponsorships"],
+  },
+]

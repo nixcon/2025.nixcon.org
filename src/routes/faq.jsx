1b39d98a81612b9e380015119b2fb7cb847e15cd
Enrico Scherlies <e.scherlies@pm.me>
2025-06-29 13:53:23 +0200

    Add table of contents for FAQ section

--- src/routes/faq.jsx
+++ src/routes/faq.jsx
@@ -8,13 +8,34 @@ renderer.link = ({ href, text }) => {
   return `<a target="_blank" rel="noopener noreferrer" href="${href}">${text}</a>`
 }
 
+renderer.heading = ({ text, depth }) => {
+  const slug = text.toLowerCase().replace(/\s+/g, "-").replace(/[?]/g, "")
+  return `<h${depth} id="${slug}">${text}</h${depth}>`
+}
+
+const tokens = marked.lexer(faqMarkdown)
+const toc = tokens.reduce((acc, token) => {
+  if (token.type === "heading" && token.depth === 2) {
+    acc.push({
+      text: token.text,
+      slug: token.text.toLowerCase().replace(/\s+/g, "-").replace(/[?]/g, ""),
+    })
+  }
+  return acc
+}, [])
+
 export default function FaqPage() {
   return (
     <div class="relative min-h-svh text-white">
       <PageLayout>
-        {/* <SubTitle>FAQ</SubTitle> */}
-
         <Section title="FAQ">
+            <ul class="space-y-2">
+              {toc.map((item) => (
+                <li class="">
+                  <a class="no-underline" href={`#${item.slug}`}>{item.text}</a>
+                </li>
+              ))}
+            </ul>
           <article
             class="prose prose-neutral prose-invert prose-p:text-white prose-li:text-white prose-li:list-['-']  prose-li:leading-4 prose-li:marker:text-white prose-headings:mt-30 lg:prose-lg"
             innerHTML={marked.parse(faqMarkdown, { async: false, gfm: true, renderer: renderer })}
